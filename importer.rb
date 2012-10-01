6dbc6e0145ad35110983c054f95d4ca8b51ce116
Rubén López <ruben@cs1.com>
2012-09-30 19:25:19 -0600

    read_data can handle an Array object or a String

--- importer.rb
+++ importer.rb
@@ -1,2 +1,34 @@
-class Importer
+class String
+  def split_and_symbolize(delimeter = ',')
+    self.split(delimeter).map do |column| 
+      column.gsub(/\s/, '_').downcase.to_sym
+    end
+  end
 end
+
+class Importer
+
+
+  attr_reader :column_names
+
+  def initialize(data, delimeter=',')
+    @column_names, @rows = *read_data(data, delimeter)
+  end
+
+  def lines
+    @rows
+  end
+
+
+  private
+
+  def read_data(data, delimeter)
+    data = data.is_a?(Array) ? data.dup : File.readlines(data).map(&:chomp)
+
+    [data.shift.split_and_symbolize, data]
+  end
+
+    
+end
+
+
--- spec/importer_spec.rb
+++ spec/importer_spec.rb
@@ -1,19 +1,40 @@
 require_relative '../importer'
 
 
-data =  [
-          "First Name,Last Name,Phone,Notes",
-          "FN_Test1,LN_Test1,111-111-1111,Hello world",
-          "FN_Test2,LN_Test2,222-222-2222,Hello world",
-          "FN_Test3,LN_Test2,333-333-3333,Hello world"
-        ]
+DUMMY_DATA =  [
+                "First Name,Last Name,Phone,Notes",
+                "FN_Test1,LN_Test1,111-111-1111,Hello world",
+                "FN_Test2,LN_Test2,222-222-2222,Hello world",
+                "FN_Test3,LN_Test2,333-333-3333,Hello world"
+              ]
 
 describe Importer do
 
-  subject { Importer.new(data, delimeter=',') }
-
   it "should have a Importer class defined?" do
     ->{ defined?(Importer) == 'constant' && Importer.is_a?(Class) }.call.should == true
+  end
+
+#  it "Responds to methods" do
+#    [:column_names].each do |method|
+#      Importer.new('file').respond_to?(method).should be_true
+#    end
+#  end
+
+  describe "New instance returns correct values" do
+    before do
+      @contacts = Importer.new(DUMMY_DATA)
+    end
+
+    it "should return the correct columns" do
+      DUMMY_DATA.first.split_and_symbolize.should == @contacts.column_names
+    end
+
+    it "should return correct amount of lines" do
+      @contacts.lines.count.should == 3
+    end
+
+
+
   end
 
 end
